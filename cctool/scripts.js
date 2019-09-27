6b4c3616a57fb35948aa267f9a9cd188f8a9345f
Emma Stellwag <emmastellwag@Emmas-MacBook-Pro.local>
2019-09-27 12:33:31 +0200

    select

---
+++ cctool/scripts.js
@@ -0,0 +1,21 @@
+fuction racechange(value){
+  var x = getElementById('cultureselect');
+  var i = 0;
+  for (i=0, i<4, i++){
+    x.remove(i+1);
+  }
+  switch(value){
+    case c:
+      break;
+    case e:
+      break;
+    case gn:
+      break;
+    case gr:
+      break;
+    case n:
+      break;
+    case z:
+      break;
+  }
+}
