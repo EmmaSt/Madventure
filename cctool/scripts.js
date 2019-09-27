bed7c9e01afa89f20d9c5a42f88cdd31d1234a57
Emma Stellwag <emmastellwag@Emmas-MBP.fritz.box>
2019-09-27 21:25:09 +0200

    c

--- cctool/scripts.js
+++ cctool/scripts.js
@@ -1,4 +1,40 @@
+var boni = {ch:0, ge:0, in:0, ko:0, st:0, we:0};
+
+racebonus(){
+  var value = race.value;
+  boni = {ch:0, ge:0, in:0, ko:0, st:0, we:0};
+  switch(value){
+    case "c":
+      boni.ge = 1;
+      break;
+    case "e":
+      boni.we = 1;
+      break;
+    case "gn":
+      boni.in = 1;
+      break;
+    case "gr":
+      boni.st = 1;
+      break;
+    case "n":
+      boni.ch = 1;
+      break;
+    case "z":
+      boni.ko = 1;
+      break;
+  }
+}
+
 function racechange(){
+  adaptcultures();
+  racebonus();
+}
+
+function culturechange(){
+
+}
+
+function adaptcultures(){
   var cul = document.getElementById('cultureselect');
   var i = 0;
   for (i=0; i<4; i++){
@@ -8,46 +44,47 @@ function racechange(){
   var value = race.value;
   switch(value){
     case "c":
-      addoption(cul,"Enchidna");
-      addoption(cul,"Schnecke");
-      addoption(cul,"Satyr");
-      addoption(cul,"Zentaur");
+      addoption(cul,"Enchidna","ench");
+      addoption(cul,"Schnecke","schn");
+      addoption(cul,"Satyr","sat");
+      addoption(cul,"Zentaur","zen");
       break;
     case "e":
-      addoption(cul,"Dunkelelfe");
-      addoption(cul,"Hochelfe");
-      addoption(cul,"Steppenelfe");
-      addoption(cul,"Waldelfe");
+      addoption(cul,"Dunkelelfe","de");
+      addoption(cul,"Hochelfe","he");
+      addoption(cul,"Steppenelfe","se");
+      addoption(cul,"Waldelfe","we");
       break;
     case "gn":
-      addoption(cul,"Felsgnom");
-      addoption(cul,"Tiefengnom");
-      addoption(cul,"Tüftelgnom");
-      addoption(cul,"Waldgnom");
+      addoption(cul,"Felsgnom","fg");
+      addoption(cul,"Tiefengnom","tig");
+      addoption(cul,"Tüftelgnom","tueg");
+      addoption(cul,"Waldgnom","wg");
       break;
     case "gr":
-      addoption(cul,"Ent");
-      addoption(cul,"Nork");
-      addoption(cul,"Riesen");
-      addoption(cul,"Zyklopen");
+      addoption(cul,"Ent","ent");
+      addoption(cul,"Nork","no");
+      addoption(cul,"Riesen","ri");
+      addoption(cul,"Zyklopen","zy");
       break;
     case "n":
-      addoption(cul,"Dryaden");
-      addoption(cul,"Erdnymphen");
-      addoption(cul,"Luftnymphen");
-      addoption(cul,"Wassernymphen");
+      addoption(cul,"Dryaden","dr");
+      addoption(cul,"Erdnymphen","erdn");
+      addoption(cul,"Luftnymphen","ln");
+      addoption(cul,"Wassernymphen","wn");
       break;
     case "z":
-      addoption(cul,"Bergzwerg");
-      addoption(cul,"Hobbitzwerg");
-      addoption(cul,"Tiefenzwerg");
-      addoption(cul,"Wickyzwerg");
+      addoption(cul,"Bergzwerg","bz");
+      addoption(cul,"Hobbitzwerg","hz");
+      addoption(cul,"Tiefenzwerg","tz");
+      addoption(cul,"Wickyzwerg","wz");
       break;
   }
 }
 
-function addoption(x, name){
+function addoption(x, name, value){
   var option = document.createElement("option");
   option.text = name;
+  option.value = value;
   x.add(option);
 }
