d91b581f8f4660677a9625226d7b3296c870a42c
Emma Stellwag <emmastellwag@Emmas-MBP.fritz.box>
2019-09-28 13:07:08 +0200

    d

--- cctool/scripts/functions.js
+++ cctool/scripts/functions.js
@@ -1,8 +1,26 @@
 var boni = {ch:0, ge:0, in:0, ko:0, st:0, we:0};
 
-function test(){
-  JSON.parse(properties.json);
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'properties.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);
+ }
+
+ function init() {
+ loadJSON(function(response) {
+  // Parse JSON string into object
+    var properties = JSON.parse(response);
+ });
 }
+
 
 function racebonus(){
   var race = document.getElementById('raceselect');
